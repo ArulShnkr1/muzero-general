dc1f57c1b4ad302d71d2a3d7614454fff640c3ac
Arul Shankar <167818716+ArulShnkr1@users.noreply.github.com>
2025-02-28 20:58:24 -0500

    Update conway_phutball.py

--- games/conway_phutball.py
+++ games/conway_phutball.py
@@ -64,7 +64,7 @@ class MuZeroConfig:
 
         # Evaluate
         self.muzero_player = 0
-        self.opponent = "expert"
+        self.opponent = "self"
 
         ### Self-Play
         self.num_workers = 1
